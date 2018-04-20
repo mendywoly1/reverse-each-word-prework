ea1950b5e05484e4ffa7d8acb964c4401cba1995
Mendy Wolosow1 <mendywoly+1@gmail.com>
2018-04-20 03:13:08 +0000

    automatically backed up by learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -6,3 +6,12 @@ def reverse_each_word(string)
   end
   arr.join(" ")
 end
+
+def reverse_each_word(string)
+  array = string.split
+  arr = []
+  array.collect do |word|
+    arr << word.reverse 
+  end
+  arr.join(" ") 
+end
